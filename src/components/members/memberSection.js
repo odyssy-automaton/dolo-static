f26763a11c734a7a091ac89c52b0cb5c0eab3c2b
oovg <office@vengist.com>
2018-11-16 14:51:54 -0800

    starts building out members lounge

--- src/components/members/memberSection.js
+++ src/components/members/memberSection.js
@@ -47,7 +47,7 @@ class memberSection extends Component {
           });
         }
       }
-      
+
     } catch (error) {
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
@@ -61,14 +61,27 @@ class memberSection extends Component {
 
     return (
       <div className="Member">
-        <div className="Member__spacer"></div>
-        <h1>Member Lounge</h1>
-        { authenticated ? 
-        (
-          <h2>Welcome Member!</h2>
-        ) : (
-          <h2>MEMBERS ONLY</h2>
-        )}
+        <div className="Member__container">
+          <h1>Member Lounge</h1>
+          { authenticated ?
+          (
+
+              <div>
+                <h2>Welcome DOLO Member!</h2>
+                <p className="large">We've compiled all current resources to assist you in participating within the DOLO Community. </p>
+                <h6>Communication Channels</h6>
+                <p><strong>Telegram</strong></p>
+                <p>Link to invite others to the public Telegram channel</p>
+                <pre>http://telegram.im</pre>
+                <p><strong>Slack</strong></p>
+                <p>Link to invite others to our Slack.</p>
+                <pre>https://join.slack.com/t/dolocommunity/shared_invite/enQtNDgxOTAzNzU2NDIwLTkxYTFlMzIwMzFjNWMxNjU1YjMzZGIyNTllYTM3YmRkOTg4YmQ0NjdhMGYxNTk3MDUyY2E3MWMwYzE4YWU0Njc</pre>
+              </div>
+
+          ) : (
+            <h2>MEMBERS ONLY</h2>
+          )}
+        </div>
       </div>
     )
   }
