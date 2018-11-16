1122c460a3bd32e7d7cc1953d164aa13320632c4
skuhlmann <samkuhlmann@gmail.com>
2018-11-16 12:39:36 -0700

    Adds some header stuff on browser check

--- src/components/shared/sign-in/signIn.js
+++ src/components/shared/sign-in/signIn.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { ClientInfo } from '../../../util/getWeb3';
+import { Web3Info, BrowserInfo } from '../../../util/getWeb3';
 
 import './signIn.scss'
 
@@ -9,44 +9,23 @@ class SignIn extends Component {
     accounts: null,
     browserInfo: {},
     web3Info: {},
+    showBrowserMessage: false,
   };
 
-  // async componentDidMount() {
-  //   try {
-  //     const clientInfo = new ClientInfo();
-  //     await clientInfo.web3Info.init();
-  //     console.log('browserinfo', clientInfo.browserInfo);
-  //     console.log('web3info', clientInfo.web3Info);
-  //     // console.log('accounts', clientInfo.web3Info.accounts);
-  //     // const accounts = clientInfo.web3Info.accounts;
-  //     this.setState({
-  //       // accounts,
-  //       browserInfo: clientInfo.browserInfo,
-  //       web3Info: clientInfo.web3Info,
-  //     });
-  //   } catch (error) {
-  //     alert(
-  //       `Failed to load web3, accounts, or contract. Check console for details.`,
-  //     );
-  //     console.log(error);
-  //   }
-
-  // }
+  componentDidMount() {
+    const browserInfo = new BrowserInfo();
+    console.log(browserInfo)
+    this.setState({
+      browserInfo
+    });
+  }
 
   signIn = async () => {
-    console.log('fuck')
-    
     try {
-      const clientInfo = new ClientInfo();
-      await clientInfo.web3Info.init();
-      console.log('browserinfo', clientInfo.browserInfo);
-      console.log('web3info', clientInfo.web3Info);
-      // console.log('accounts', clientInfo.web3Info.accounts);
-      // const accounts = clientInfo.web3Info.accounts;
+      const web3Info = new Web3Info();
+      await web3Info.init();
       this.setState({
-        // accounts,
-        browserInfo: clientInfo.browserInfo,
-        web3Info: clientInfo.web3Info,
+        web3Info
       });
     } catch (error) {
       alert(
@@ -54,12 +33,33 @@ class SignIn extends Component {
       );
       console.log(error);
     }
+  }
 
+  toggleMessage = () => {
+    if (!this.state.browserInfo.metaMaskSupport) {
+      this.setState({
+        showBrowserMessage: !this.state.showBrowserMessage,
+      })
+    }
   }
 
   render() {
+    const { browserInfo, showBrowserMessage } = this.state;
+    const circleClass = (browserInfo && browserInfo.metaMaskSupport) 
+      ? "Member__circle enabled" 
+      : "Member__circle not-enabled";
+
     return (
-      <span onClick={this.signIn}>Sign In</span>
+      <div onMouseEnter={this.toggleMessage} onMouseLeave={this.toggleMessage}>
+        <span onClick={this.signIn}>
+          Members Lounge
+          <div className={circleClass}></div>
+        </span>
+        { showBrowserMessage ? 
+          <p className="Member-warning">Use a better browser, buster</p>
+          : null
+        }
+      </div>
     )
   }
 
--- src/util/getWeb3.js
+++ src/util/getWeb3.js
@@ -1,166 +1,51 @@
 import { ethers } from 'ethers';
 
-export class ClientInfo {
-  constructor() {
-    this.browserInfo = new BrowserInfo();
-    this.web3Info = new Web3Info();
-  }
-}
-
 export class Web3Info {
-  getProviderInfo(web3) {
-    this.isMetammask = web3.currentProvider.isMetaMask;
-    this.isTrust = web3.currentProvider.isTrust;
-    this.isToshi = typeof window.SOFA !== 'undefined';
-    this.isCipher = typeof window.__CIPHER__ !== 'undefined';
-    this.isMist = web3.currentProvider.constructor.name === 'EthereumProvider';
-    this.isParity =
-      web3.currentProvider.constructor.name === 'Web3FrameProvider';
-    this.isInfura =
-      web3.currentProvider.host &&
-      window.web3.currentProvider.host.indexOf('infura') !== -1;
-    this.isLocalhost =
-      window.web3.currentProvider.host &&
-      window.web3.currentProvider.host.indexOf('localhost') !== -1;
-
-    this.isUnknown =
-      !this.isMetammask &&
-      !this.isTrust &&
-      !this.isToshi &&
-      !this.isCipher &&
-      !this.isMist &&
-      !this.isParity &&
-      !this.isInfura &&
-      !this.isLocalhost;
-  }
-
-  async getAccountInfo(web3) {
-    this.accounts = await web3.eth.getAccounts();
-    if (this.accounts && this.accounts.length) {
-      this.loggedIn = true;
-      this.balance = await web3.eth.getBalance(this.accounts[0]);
-      console.log('logged in!');
-    } else if (this.accounts && !this.accounts.length) {
-      console.log('logged out or privacy mode');
-
-      this.loggedIn = false;
-    } else {
-      console.log('logged out or privacy mode');
-      console.log('no accounts from web3. what does this mean? ');
-      this.loggedIn = false;
-    }
-  }
-
-  async getNetworkInfo(web3) {
-    this.network = '';
-    this.networkId = await web3.eth.net.getId();
-    this.networkType = await web3.eth.net.getNetworkType();
-    console.log('network', this.network);
-  }
-
-  getcurrentProvider(web3) {
-    debugger;
-    this.provider = web3.currentProvider;
-    console.log('provider', this.provider);
-  }
-
   checkWeb3andFallback(web3, resolve, reject) {
-    // Checking if Web3 has been injected by the browser (Mist/MetaMask).
     const alreadyInjected = typeof web3 !== 'undefined';
 
     if (alreadyInjected) {
-      // Use Mist/MetaMask's provider.
-      // web3 = new Web3(web3.currentProvider);
-
-      console.log(web3.currentProvider)
       const provider = new ethers.providers.Web3Provider(web3.currentProvider);
       const signer = provider.getSigner();
-      signer.signMessage ( 'poopin' )
 
-      // console.log()
-      // web3 = new ethers.providers.Web3Provider(web3.currentProvider);
-      console.log('Injected web3 detected.');
-      console.log(provider);
+      signer.signMessage('poopin')
+
       resolve(provider);
     } else {
-
-      
-      // Fallback to localhost if no web3 injection. We've configured this to
-      // use the development console's port by default.
-      // should use infura instead?
-      // const provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
-      // const provider = new web3.providers.HttpProvider('http://localhost:8545');
-      // web3 = new Web3(provider);
-      // web3 = new ethers.providers.Web3Provider(web3.currentProvider);
-      const provider = ethers.providers.getDefaultProvider();
       console.log('No web3 instance injected, using Local web3.');
+      //do they need to sign?
+      const provider = ethers.providers.getDefaultProvider();
       resolve(provider);
     }
   }
 
   getWeb3() {
-
-    console.log('in getweb3')
     return new Promise((resolve, reject) => {
-      // Wait for loading completion to avoid race conditions with web3 injection timing.
       let web3 = undefined;
-      
-      // window.addEventListener('load', () => {
-        if (window.ethereum) {
-          // does this check for new metamask
-          // what to do when rejected
-          window.ethereum
-            .enable()
-            .then((arg1) => {
-              console.log('user approves', arg1);
-              web3 = window.web3;
-              this.checkWeb3andFallback(web3, resolve, reject);
-            })
-            .catch((arg1) => {
-              console.log('user disaproves', arg1);
-              reject();
-            });
-        } else {
-          web3 = window.web3;
-          this.checkWeb3andFallback(web3, resolve, reject);
-        }
-      // });
+      if (window.ethereum) {
+        window.ethereum
+          .enable()
+          .then((arg1) => {
+            web3 = window.web3;
+            this.checkWeb3andFallback(web3, resolve, reject);
+          })
+          .catch((arg1) => {
+            reject();
+          });
+      } else {
+        web3 = window.web3;
+        this.checkWeb3andFallback(web3, resolve, reject);
+      }
     });
   }
 
   async init() {
-    console.log('init')
-    //TODO: update web3Service and update checks if logged in or no metamask use infura or local
     this.web3 = this.web3 || (await this.getWeb3());
-
-    console.log('init')
-    console.log(this.web3)
-
-    const accounts = this.web3.listAccounts()
-                      .then(result => console.log(result))
-                      .catch(error => console.log(error))
-    // this.getcurrentProvider(this.web3);
-    // this.getProviderInfo(this.web3);
-    // await this.getAccountInfo(this.web3);
-    // await this.getNetworkInfo(this.web3);
-    // console.log('publics', this.accounts, this.networkId);
-
-    // //add event listeners
-    // const updateCallback = (change) => {
-    //   if (
-    //     this.accounts[0].toString().toUpperCase() !==
-    //     change.selectedAddress.toUpperCase()
-    //   ) {
-    //     console.log('address changed');
-    //     window.location.reload();
-    //   }
-    //   // does not work will need case staement to guess id
-    //   // if (this.network !== change.networkVersion) {
-    //   //   console.log('network changed');
-    //   // }
-    // };
-
-    // this.web3.currentProvider.publicConfigStore.on('update', updateCallback);
+    this.web3.listAccounts()
+      .then(result => {
+        this.accounts = result; 
+      })
+      .catch(error => console.log(error));
   }
 }
 
