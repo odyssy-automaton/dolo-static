aa9d7b29cd7dd79daf750923be88a0aae5350474
oovg <office@vengist.com>
2018-11-16 16:05:39 -0800

    starter layout and content

--- src/components/members/memberSection.js
+++ src/components/members/memberSection.js
@@ -66,32 +66,48 @@ class memberSection extends Component {
     return (
       <div className="Member">
         <div className="Member__container">
-          <h1>Member Lounge</h1>
+          <div className="Member__hero">
+            <h1>Member Lounge</h1>
+          </div>
           { authenticated && !loading ?
           (
+              <div className="Member__Validated">
+                <div className="Member__Validated--hero">
+                  <h2>Hey there DOLO Member!</h2>
+                  <p className="large">We've compiled all current resources to assist you in participating within the DOLO Community. </p>
+                </div>
+                <div className="Member__Contents">
+                  <h6>Resources</h6>
+                  <p><strong>Telegram</strong> is used as our public channel, serving as the lobby for those outside of DOLO to communicate with members. Share the link below to invite others to the Telegram.</p>
+                  <a href="https://t.me/joinchat/IJqu9w9GgBScrKbU0bCRLA" target="_blank">Go to Telegram</a>
+                  <pre>https://t.me/joinchat/IJqu9w9GgBScrKbU0bCRLA</pre>
 
-              <div>
-                <h2>Welcome DOLO Member!</h2>
-                <p className="large">We've compiled all current resources to assist you in participating within the DOLO Community. </p>
-                <h6>Communication Channels</h6>
-                <p><strong>Telegram</strong></p>
-                <p>Link to invite others to the public Telegram channel</p>
-                <pre>http://telegram.im</pre>
-                <p><strong>Slack</strong></p>
-                <p>Link to invite others to our Slack.</p>
-                <pre>https://join.slack.com/t/dolocommunity/shared_invite/enQtNDgxOTAzNzU2NDIwLTkxYTFlMzIwMzFjNWMxNjU1YjMzZGIyNTllYTM3YmRkOTg4YmQ0NjdhMGYxNTk3MDUyY2E3MWMwYzE4YWU0Njc</pre>
+                  <p><strong>Slack</strong> is the main method for members to communicate and collaborate with other members of the DOLO community. Share the link below to invite others to our Slack.</p>
+                  <a href="https://bit.ly/2DFYz6l" target="_blank">Go to Slack</a>
+                  <pre>https://bit.ly/2DFYz6l</pre>
+
+                  <p><strong>Google Drive</strong> is used for the file storage and sharing, as well as document creation and collaborative editing.</p>
+                  <a href="https://drive.google.com/drive/folders/1kRtQ5usq4f9mow4uLOd4z8hroTayb6TM?usp=sharing" target="_blank">Go to DOLO Drive</a>
+                  <pre>https://drive.google.com/drive/folders/1kRtQ5usq4f9mow4uLOd4z8hroTayb6TM?usp=sharing</pre>
+
+                </div>
               </div>
 
+          ) : !loading ? (
+            <div className="members-only">
+              <h2>MEMBERS ONLY</h2>
+              <p className="large">This area can only be accessed by members of the DOLO community.</p>
+              <p>To join our community, go to <a target="_blank" href="https://app.nativeproject.one">Native</a>.</p>
+            </div>
           ) : (
-            <h2>MEMBERS ONLY</h2>
-          ) : (
-            <h2>Loading...</h2>
+            <div className="loading">
+              <h2>Validating Member Status ...</h2>
+            </div>
           )}
         </div>
       </div>
     )
   }
 }
-
 
 export default memberSection;
